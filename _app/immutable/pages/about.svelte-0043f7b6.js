47d10e9d5d5acbd059046ca562db48e0f389f185
nikitave <66897293+nikitave@users.noreply.github.com>
2022-07-08 11:09:34 +0000

    Deploying to gh-pages from @ nikitave/sveltekit@10ff732ad9be4a8ca2dba68a27492daafab0fbd8 🚀

---
+++ _app/immutable/pages/about.svelte-0043f7b6.js
@@ -0,0 +1,117 @@
+import{S as Wi,i as Ji,s as Xi,e as o,c as u,b,K as Zi,g as xe,n as me,d as s,k as m,a as l,m as c,J as e,t as d,h as f,L as Ti,x as ce,y as pe,z as de,r as fe,p as ve,C as be}from"../chunks/index-396b3103.js";import{F as et}from"../chunks/Footer-a55f87d6.js";import{L as it,H as tt}from"../chunks/Links-32f9f0c8.js";import"../chunks/paths-396f020f.js";function Yi(q,i,r){const v=q.slice();return v[2]=i[r],v}function Bi(q,i,r){const v=q.slice();return v[5]=i[r],v}function Gi(q){let i,r;return{c(){i=o("img"),this.h()},l(v){i=u(v,"IMG",{class:!0,src:!0,alt:!0}),this.h()},h(){b(i,"class","big_image svelte-80xwx8"),Zi(i.src,r=q[5].src)||b(i,"src",r),b(i,"alt","Wait for a moment, please")},m(v,g){xe(v,i,g)},p:me,d(v){v&&s(i)}}}function Ki(q){let i,r,v,g,k;return{c(){i=o("a"),r=o("img"),g=m(),this.h()},l(x){i=u(x,"A",{href:!0,target:!0});var A=l(i);r=u(A,"IMG",{class:!0,src:!0,alt:!0}),g=c(A),A.forEach(s),this.h()},h(){b(r,"class","icon svelte-80xwx8"),Zi(r.src,v=q[2].src)||b(r,"src",v),b(r,"alt","Wait for a moment, please"),b(i,"href",k=q[2].href),b(i,"target","_blank")},m(x,A){xe(x,i,A),e(i,r),e(i,g)},p:me,d(x){x&&s(i)}}}function at(q){let i,r,v,g,k,x,A,I,S,h,E,C,he,qe,R,t,T,ge,_e,Y,we,Ee,B,Ae,$e,G,Ie,Se,K,Ne,Pe,U,ke,De,W,ye,Le,J,Ce,Me,X,Ve,Fe,Z,je,ze,ee,He,Qe,ie,Oe,Re,te,Te,Ye,ae,Be,Ge,se,Ke,Ue,ne,We,Je,oe,Xe,Ze,ue,ei,ii,re,ti,ai,le,si,ni,N,M,oi,ui,V,ri,li,mi,P,F,ci,pi,j,di,fi,y,z=q[0],_=[];for(let p=0;p<z.length;p+=1)_[p]=Gi(Bi(q,z,p));let H=q[1],w=[];for(let p=0;p<H.length;p+=1)w[p]=Ki(Yi(q,H,p));return{c(){i=o("main"),r=o("div"),v=o("h2"),g=d("About me"),k=m(),x=o("p"),A=d("I am 18 years old"),I=m(),S=o("p"),h=d("I am Frond-end web developer"),E=m(),C=o("p"),he=d("I am a student of Innopolis university"),qe=m(),R=o("div"),t=o("p"),T=o("span"),ge=d(`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis
+          quis corporis perferendis et, ipsum assumenda ea asperiores eius non
+          quam beatae explicabo nesciunt pariatur recusandae voluptas distinctio
+          tenetur sint ipsa?`),_e=m(),Y=o("span"),we=d(`Doloremque laboriosam, quis officia officiis in eligendi autem
+          perspiciatis. Aliquid veniam, sit soluta iste corrupti voluptates
+          omnis, est quaerat quis commodi esse eius. Corporis voluptas, delectus
+          accusantium quis nobis cum!`),Ee=m(),B=o("span"),Ae=d(`Odio, voluptate, asperiores. Sed, similique, tempore? Dolor
+          asperiores fugit est nihil ipsam illum quis distinctio quas quia quos
+          minus possimus odit impedit velit doloribus beatae necessitatibus quae
+          ex, mollitia unde.`),$e=m(),G=o("span"),Ie=d(`Ipsa vel ullam sint fugit ea eum, vero optio cum nisi saepe maiores
+          explicabo recusandae ipsam voluptatum libero veniam accusantium, amet
+          dolorum eveniet, quas itaque esse omnis temporibus voluptatibus.
+          Distinctio.`),Se=m(),K=o("span"),Ne=d(`Laboriosam quisquam maiores ullam itaque nihil, enim dolor neque
+          nostrum! Asperiores, facilis, ut dolorem temporibus nobis voluptate
+          quasi, recusandae veniam repudiandae quam libero iusto eligendi
+          necessitatibus magni provident doloremque officiis!`),Pe=m(),U=o("span"),ke=d(`Temporibus debitis expedita recusandae sunt porro, commodi veritatis
+          explicabo sint officia doloribus suscipit sed facere reiciendis ipsum
+          maxime voluptates blanditiis molestiae quisquam atque. Quo ex labore
+          eos odit laborum molestias!`),De=m(),W=o("span"),ye=d(`Labore a facere aspernatur magni at, voluptatum quam ipsum
+          accusantium, in fugiat beatae harum. Eligendi labore atque eos quis
+          odit dignissimos hic repudiandae, saepe, cupiditate incidunt nisi ex
+          rem nostrum.`),Le=m(),J=o("span"),Ce=d(`Eum quod quas explicabo fugiat modi autem, ex rerum. Iusto labore
+          commodi architecto unde voluptate ratione numquam officia, doloribus
+          dolores repellendus, alias eum rerum odio inventore mollitia tenetur
+          vitae nisi!`),Me=m(),X=o("span"),Ve=d(`Sapiente at, sunt, sequi ducimus suscipit nostrum maxime
+          necessitatibus minima maiores possimus numquam! Laboriosam incidunt,
+          sequi nihil earum? Dolores nobis quasi nisi minus quia laborum
+          doloremque vero aliquid odit assumenda.`),Fe=m(),Z=o("span"),je=d(`Dolor ex, rerum harum maxime rem necessitatibus maiores unde ipsam
+          molestias ipsum eaque earum, consectetur accusamus dolorem illo, quam,
+          vitae amet nesciunt. Consequatur, maxime, sit. Impedit corporis nam
+          labore ut.`),ze=m(),ee=o("span"),He=d(`Itaque aspernatur maxime, dicta quos, distinctio error, veritatis
+          commodi modi culpa officia explicabo at ratione reprehenderit eum
+          sapiente iusto quisquam corporis voluptatibus architecto eaque rerum
+          nihil odit, amet! Ad, vitae?`),Qe=m(),ie=o("span"),Oe=d(`Incidunt at a sequi, blanditiis totam. Fugit ab quibusdam dolores
+          optio natus aperiam nesciunt neque numquam facere dolorem qui
+          molestias illo perferendis, magni ipsum vero facilis aspernatur
+          commodi voluptates. Est.`),Re=m(),te=o("span"),Te=d(`Explicabo laudantium adipisci ratione, labore odit! Consequuntur,
+          porro ex voluptatem aspernatur pariatur debitis minus et voluptates
+          omnis natus magnam possimus eius in ipsam quaerat quod ipsum quae
+          veniam minima earum.`),Ye=m(),ae=o("span"),Be=d(`Voluptatum totam accusamus, nihil veritatis dolorum beatae ratione,
+          delectus rerum. Rem, a illum autem ipsam soluta eos nemo, repudiandae
+          eligendi, ea incidunt et enim? Nobis quos voluptatem sunt, odio rerum.`),Ge=m(),se=o("span"),Ke=d(`Molestias, fuga earum repellat expedita. Mollitia esse saepe
+          assumenda facere ducimus fugiat quos suscipit tempora odit labore
+          doloremque ipsum magni magnam asperiores officia, molestiae temporibus
+          fugit doloribus adipisci. Atque, dolor.`),Ue=m(),ne=o("span"),We=d(`Autem cum consequatur cupiditate dicta error reiciendis nam ullam
+          excepturi consectetur quam doloremque, adipisci labore consequuntur
+          quibusdam beatae ipsum sint repellat at, laudantium a temporibus
+          maxime blanditiis? Officiis, quia, accusamus.`),Je=m(),oe=o("span"),Xe=d(`Nam exercitationem eligendi debitis, nostrum quo laudantium! Quam
+          enim, nisi, quidem explicabo, placeat rem reiciendis error, mollitia
+          nobis corporis nihil possimus reprehenderit ullam. Fugit blanditiis
+          aperiam et qui accusantium, hic.`),Ze=m(),ue=o("span"),ei=d(`Sint officiis saepe at veritatis voluptatem error consectetur facilis
+          illum laborum, ab nostrum eius, vitae nisi dolor eum dolorem. Illo sit
+          praesentium, vel nisi nobis eveniet delectus voluptatem ullam numquam.`),ii=m(),re=o("span"),ti=d(`Recusandae autem veniam totam soluta illo odio maiores eos accusamus,
+          ducimus ab, corporis magni ea ipsam sunt laborum aliquid
+          reprehenderit. Tempore laborum repellendus debitis placeat sint, ut
+          est ipsam adipisci?`),ai=m(),le=o("span"),si=d(`Voluptate ad unde a, totam recusandae? Quasi, necessitatibus magnam
+          dignissimos nisi pariatur. Ut vitae necessitatibus esse, laborum, ad
+          delectus eum optio voluptate magni pariatur fugit quia vel quaerat.
+          Error, eum?`),ni=m(),N=o("div"),M=o("h2"),oi=d("My works"),ui=m(),V=o("p"),ri=d("Here is some of my works in web:"),li=m();for(let p=0;p<_.length;p+=1)_[p].c();mi=m(),P=o("div"),F=o("h2"),ci=d("Contact me"),pi=m(),j=o("p"),di=d("You can contact me here:"),fi=m(),y=o("div");for(let p=0;p<w.length;p+=1)w[p].c();this.h()},l(p){i=u(p,"MAIN",{});var $=l(i);r=u($,"DIV",{class:!0});var a=l(r);v=u(a,"H2",{class:!0});var D=l(v);g=f(D,"About me"),D.forEach(s),k=c(a),x=u(a,"P",{class:!0});var vi=l(x);A=f(vi,"I am 18 years old"),vi.forEach(s),I=c(a),S=u(a,"P",{class:!0});var bi=l(S);h=f(bi,"I am Frond-end web developer"),bi.forEach(s),E=c(a),C=u(a,"P",{class:!0});var xi=l(C);he=f(xi,"I am a student of Innopolis university"),xi.forEach(s),qe=c(a),R=u(a,"DIV",{});var hi=l(R);t=u(hi,"P",{class:!0});var n=l(t);T=u(n,"SPAN",{});var qi=l(T);ge=f(qi,`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis
+          quis corporis perferendis et, ipsum assumenda ea asperiores eius non
+          quam beatae explicabo nesciunt pariatur recusandae voluptas distinctio
+          tenetur sint ipsa?`),qi.forEach(s),_e=c(n),Y=u(n,"SPAN",{});var gi=l(Y);we=f(gi,`Doloremque laboriosam, quis officia officiis in eligendi autem
+          perspiciatis. Aliquid veniam, sit soluta iste corrupti voluptates
+          omnis, est quaerat quis commodi esse eius. Corporis voluptas, delectus
+          accusantium quis nobis cum!`),gi.forEach(s),Ee=c(n),B=u(n,"SPAN",{});var _i=l(B);Ae=f(_i,`Odio, voluptate, asperiores. Sed, similique, tempore? Dolor
+          asperiores fugit est nihil ipsam illum quis distinctio quas quia quos
+          minus possimus odit impedit velit doloribus beatae necessitatibus quae
+          ex, mollitia unde.`),_i.forEach(s),$e=c(n),G=u(n,"SPAN",{});var wi=l(G);Ie=f(wi,`Ipsa vel ullam sint fugit ea eum, vero optio cum nisi saepe maiores
+          explicabo recusandae ipsam voluptatum libero veniam accusantium, amet
+          dolorum eveniet, quas itaque esse omnis temporibus voluptatibus.
+          Distinctio.`),wi.forEach(s),Se=c(n),K=u(n,"SPAN",{});var Ei=l(K);Ne=f(Ei,`Laboriosam quisquam maiores ullam itaque nihil, enim dolor neque
+          nostrum! Asperiores, facilis, ut dolorem temporibus nobis voluptate
+          quasi, recusandae veniam repudiandae quam libero iusto eligendi
+          necessitatibus magni provident doloremque officiis!`),Ei.forEach(s),Pe=c(n),U=u(n,"SPAN",{});var Ai=l(U);ke=f(Ai,`Temporibus debitis expedita recusandae sunt porro, commodi veritatis
+          explicabo sint officia doloribus suscipit sed facere reiciendis ipsum
+          maxime voluptates blanditiis molestiae quisquam atque. Quo ex labore
+          eos odit laborum molestias!`),Ai.forEach(s),De=c(n),W=u(n,"SPAN",{});var $i=l(W);ye=f($i,`Labore a facere aspernatur magni at, voluptatum quam ipsum
+          accusantium, in fugiat beatae harum. Eligendi labore atque eos quis
+          odit dignissimos hic repudiandae, saepe, cupiditate incidunt nisi ex
+          rem nostrum.`),$i.forEach(s),Le=c(n),J=u(n,"SPAN",{});var Ii=l(J);Ce=f(Ii,`Eum quod quas explicabo fugiat modi autem, ex rerum. Iusto labore
+          commodi architecto unde voluptate ratione numquam officia, doloribus
+          dolores repellendus, alias eum rerum odio inventore mollitia tenetur
+          vitae nisi!`),Ii.forEach(s),Me=c(n),X=u(n,"SPAN",{});var Si=l(X);Ve=f(Si,`Sapiente at, sunt, sequi ducimus suscipit nostrum maxime
+          necessitatibus minima maiores possimus numquam! Laboriosam incidunt,
+          sequi nihil earum? Dolores nobis quasi nisi minus quia laborum
+          doloremque vero aliquid odit assumenda.`),Si.forEach(s),Fe=c(n),Z=u(n,"SPAN",{});var Ni=l(Z);je=f(Ni,`Dolor ex, rerum harum maxime rem necessitatibus maiores unde ipsam
+          molestias ipsum eaque earum, consectetur accusamus dolorem illo, quam,
+          vitae amet nesciunt. Consequatur, maxime, sit. Impedit corporis nam
+          labore ut.`),Ni.forEach(s),ze=c(n),ee=u(n,"SPAN",{});var Pi=l(ee);He=f(Pi,`Itaque aspernatur maxime, dicta quos, distinctio error, veritatis
+          commodi modi culpa officia explicabo at ratione reprehenderit eum
+          sapiente iusto quisquam corporis voluptatibus architecto eaque rerum
+          nihil odit, amet! Ad, vitae?`),Pi.forEach(s),Qe=c(n),ie=u(n,"SPAN",{});var ki=l(ie);Oe=f(ki,`Incidunt at a sequi, blanditiis totam. Fugit ab quibusdam dolores
+          optio natus aperiam nesciunt neque numquam facere dolorem qui
+          molestias illo perferendis, magni ipsum vero facilis aspernatur
+          commodi voluptates. Est.`),ki.forEach(s),Re=c(n),te=u(n,"SPAN",{});var Di=l(te);Te=f(Di,`Explicabo laudantium adipisci ratione, labore odit! Consequuntur,
+          porro ex voluptatem aspernatur pariatur debitis minus et voluptates
+          omnis natus magnam possimus eius in ipsam quaerat quod ipsum quae
+          veniam minima earum.`),Di.forEach(s),Ye=c(n),ae=u(n,"SPAN",{});var yi=l(ae);Be=f(yi,`Voluptatum totam accusamus, nihil veritatis dolorum beatae ratione,
+          delectus rerum. Rem, a illum autem ipsam soluta eos nemo, repudiandae
+          eligendi, ea incidunt et enim? Nobis quos voluptatem sunt, odio rerum.`),yi.forEach(s),Ge=c(n),se=u(n,"SPAN",{});var Li=l(se);Ke=f(Li,`Molestias, fuga earum repellat expedita. Mollitia esse saepe
+          assumenda facere ducimus fugiat quos suscipit tempora odit labore
+          doloremque ipsum magni magnam asperiores officia, molestiae temporibus
+          fugit doloribus adipisci. Atque, dolor.`),Li.forEach(s),Ue=c(n),ne=u(n,"SPAN",{});var Ci=l(ne);We=f(Ci,`Autem cum consequatur cupiditate dicta error reiciendis nam ullam
+          excepturi consectetur quam doloremque, adipisci labore consequuntur
+          quibusdam beatae ipsum sint repellat at, laudantium a temporibus
+          maxime blanditiis? Officiis, quia, accusamus.`),Ci.forEach(s),Je=c(n),oe=u(n,"SPAN",{});var Mi=l(oe);Xe=f(Mi,`Nam exercitationem eligendi debitis, nostrum quo laudantium! Quam
+          enim, nisi, quidem explicabo, placeat rem reiciendis error, mollitia
+          nobis corporis nihil possimus reprehenderit ullam. Fugit blanditiis
+          aperiam et qui accusantium, hic.`),Mi.forEach(s),Ze=c(n),ue=u(n,"SPAN",{});var Vi=l(ue);ei=f(Vi,`Sint officiis saepe at veritatis voluptatem error consectetur facilis
+          illum laborum, ab nostrum eius, vitae nisi dolor eum dolorem. Illo sit
+          praesentium, vel nisi nobis eveniet delectus voluptatem ullam numquam.`),Vi.forEach(s),ii=c(n),re=u(n,"SPAN",{});var Fi=l(re);ti=f(Fi,`Recusandae autem veniam totam soluta illo odio maiores eos accusamus,
+          ducimus ab, corporis magni ea ipsam sunt laborum aliquid
+          reprehenderit. Tempore laborum repellendus debitis placeat sint, ut
+          est ipsam adipisci?`),Fi.forEach(s),ai=c(n),le=u(n,"SPAN",{});var ji=l(le);si=f(ji,`Voluptate ad unde a, totam recusandae? Quasi, necessitatibus magnam
+          dignissimos nisi pariatur. Ut vitae necessitatibus esse, laborum, ad
+          delectus eum optio voluptate magni pariatur fugit quia vel quaerat.
+          Error, eum?`),ji.forEach(s),n.forEach(s),hi.forEach(s),a.forEach(s),ni=c($),N=u($,"DIV",{class:!0});var Q=l(N);M=u(Q,"H2",{class:!0});var zi=l(M);oi=f(zi,"My works"),zi.forEach(s),ui=c(Q),V=u(Q,"P",{class:!0});var Hi=l(V);ri=f(Hi,"Here is some of my works in web:"),Hi.forEach(s),li=c(Q);for(let L=0;L<_.length;L+=1)_[L].l(Q);Q.forEach(s),mi=c($),P=u($,"DIV",{class:!0});var O=l(P);F=u(O,"H2",{class:!0});var Qi=l(F);ci=f(Qi,"Contact me"),Qi.forEach(s),pi=c(O),j=u(O,"P",{class:!0});var Oi=l(j);di=f(Oi,"You can contact me here:"),Oi.forEach(s),fi=c(O),y=u(O,"DIV",{class:!0});var Ri=l(y);for(let L=0;L<w.length;L+=1)w[L].l(Ri);Ri.forEach(s),O.forEach(s),$.forEach(s),this.h()},h(){b(v,"class","title block_animation svelte-80xwx8"),b(x,"class","just_text svelte-80xwx8"),b(S,"class","just_text m1 svelte-80xwx8"),b(C,"class","just_text m2 svelte-80xwx8"),b(t,"class","text svelte-80xwx8"),b(r,"class","block_info svelte-80xwx8"),b(M,"class","title svelte-80xwx8"),b(V,"class","just_text svelte-80xwx8"),b(N,"class","block_info svelte-80xwx8"),b(F,"class","title svelte-80xwx8"),b(j,"class","just_text md svelte-80xwx8"),b(y,"class","icons svelte-80xwx8"),b(P,"class","block_info svelte-80xwx8")},m(p,$){xe(p,i,$),e(i,r),e(r,v),e(v,g),e(r,k),e(r,x),e(x,A),e(r,I),e(r,S),e(S,h),e(r,E),e(r,C),e(C,he),e(r,qe),e(r,R),e(R,t),e(t,T),e(T,ge),e(t,_e),e(t,Y),e(Y,we),e(t,Ee),e(t,B),e(B,Ae),e(t,$e),e(t,G),e(G,Ie),e(t,Se),e(t,K),e(K,Ne),e(t,Pe),e(t,U),e(U,ke),e(t,De),e(t,W),e(W,ye),e(t,Le),e(t,J),e(J,Ce),e(t,Me),e(t,X),e(X,Ve),e(t,Fe),e(t,Z),e(Z,je),e(t,ze),e(t,ee),e(ee,He),e(t,Qe),e(t,ie),e(ie,Oe),e(t,Re),e(t,te),e(te,Te),e(t,Ye),e(t,ae),e(ae,Be),e(t,Ge),e(t,se),e(se,Ke),e(t,Ue),e(t,ne),e(ne,We),e(t,Je),e(t,oe),e(oe,Xe),e(t,Ze),e(t,ue),e(ue,ei),e(t,ii),e(t,re),e(re,ti),e(t,ai),e(t,le),e(le,si),e(i,ni),e(i,N),e(N,M),e(M,oi),e(N,ui),e(N,V),e(V,ri),e(N,li);for(let a=0;a<_.length;a+=1)_[a].m(N,null);e(i,mi),e(i,P),e(P,F),e(F,ci),e(P,pi),e(P,j),e(j,di),e(P,fi),e(P,y);for(let a=0;a<w.length;a+=1)w[a].m(y,null)},p(p,[$]){if($&1){z=p[0];let a;for(a=0;a<z.length;a+=1){const D=Bi(p,z,a);_[a]?_[a].p(D,$):(_[a]=Gi(D),_[a].c(),_[a].m(N,null))}for(;a<_.length;a+=1)_[a].d(1);_.length=z.length}if($&2){H=p[1];let a;for(a=0;a<H.length;a+=1){const D=Yi(p,H,a);w[a]?w[a].p(D,$):(w[a]=Ki(D),w[a].c(),w[a].m(y,null))}for(;a<w.length;a+=1)w[a].d(1);w.length=H.length}},i:me,o:me,d(p){p&&s(i),Ti(_,p),Ti(w,p)}}}function st(q){return[[{src:"./blog_BN_photo.png"},{src:"./blog_BK_photo.png"},{src:"./blog_one_photo.png"}],[{href:"https://vk.com/n.bogdankov1",src:"https://avatars.mds.yandex.net/i?id=5e1032ba095cb91973db14de5dba0d7c-5859086-images-thumbs&n=13&exp=1"},{href:"https://github.com/nikitave",src:"https://avatars.mds.yandex.net/i?id=af50b0060de40746d74bb003de7fe5a6-5858460-images-thumbs&n=13&exp=1"},{href:"https://t.me/nikibog",src:"https://avatars.mds.yandex.net/i?id=37ce9d15c9cfc3c15ee25667bf0e8dcb-4872083-images-thumbs&n=13&exp=1"}]]}class nt extends Wi{constructor(i){super(),Ji(this,i,st,at,Xi,{})}}function ot(q){let i,r,v,g,k,x,A,I,S;return r=new it({props:{condition:Ui}}),g=new tt({props:{condition:Ui}}),x=new nt({}),I=new et({}),{c(){i=o("main"),ce(r.$$.fragment),v=m(),ce(g.$$.fragment),k=m(),ce(x.$$.fragment),A=m(),ce(I.$$.fragment)},l(h){i=u(h,"MAIN",{});var E=l(i);pe(r.$$.fragment,E),v=c(E),pe(g.$$.fragment,E),k=c(E),pe(x.$$.fragment,E),A=c(E),pe(I.$$.fragment,E),E.forEach(s)},m(h,E){xe(h,i,E),de(r,i,null),e(i,v),de(g,i,null),e(i,k),de(x,i,null),e(i,A),de(I,i,null),S=!0},p:me,i(h){S||(fe(r.$$.fragment,h),fe(g.$$.fragment,h),fe(x.$$.fragment,h),fe(I.$$.fragment,h),S=!0)},o(h){ve(r.$$.fragment,h),ve(g.$$.fragment,h),ve(x.$$.fragment,h),ve(I.$$.fragment,h),S=!1},d(h){h&&s(i),be(r),be(g),be(x),be(I)}}}let Ui=3;class ct extends Wi{constructor(i){super(),Ji(this,i,null,ot,Xi,{})}}export{ct as default};
